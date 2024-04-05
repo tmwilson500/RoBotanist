202bc18371c2d57427ae8291b37f198feadccf6d
tmwilson500 <44278526+tmwilson500@users.noreply.github.com>
2024-04-04 21:54:06 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,12 @@
+import os
+from Plant import Plant
 
 
-msg = "stuff"
-print(msg)
+#Clear terminal before running any code 
+os.system('cls')
+
+aloe = Plant(ID=1, schedule=1, name="aloemar")
+
+print("ID is: ", aloe.ID)
+print("Schedule is: ", aloe.schedule)
+print("Name is: ", aloe.name)
